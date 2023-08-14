85fc2675673c10841de15532b69c41165a8d6fb6
TasneemArshad <105126306+Tasneem378@users.noreply.github.com>
2023-08-14 12:25:47 +0500

    Update Q5.js

--- Q5.js
+++ Q5.js
@@ -1,4 +1,4 @@
 //Create a set of 5 string elements from array
-let str=['sara','sania','sana','samina','saima'];
+let str=['sara','sania','sana','samina','saima','Amna'];
 setofstr= new Set(str);
 console.log(setofstr);
